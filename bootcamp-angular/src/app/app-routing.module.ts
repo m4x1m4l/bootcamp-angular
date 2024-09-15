4ae8b0b004bae8eddd72d3b51e0186d5a8c882d6
Max Grimm <max.grimm@stud.h-da.de>
2024-09-15 12:01:19 +0200

    angular material, bootstrap, nav with routing

--- bootcamp-angular/src/app/app-routing.module.ts
+++ bootcamp-angular/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmployeeComponent} from "./employee/employee.component";
+import {TeamComponent} from "./team/team.component";
+import {SkillComponent} from "./skill/skill.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employee', component: EmployeeComponent
+  },
+  {
+    path: 'team', component: TeamComponent
+  },
+  {
+    path: 'skill', component: SkillComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
