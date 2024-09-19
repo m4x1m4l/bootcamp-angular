1e9c01a4cf28b61753dc19903badffe2771cd7e0
Max Grimm <max.grimm@stud.h-da.de>
2024-09-19 12:41:57 +0200

    service implementiert

--- bootcamp-angular/src/app/page/team/service/team.service.ts
+++ bootcamp-angular/src/app/page/team/service/team.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject, catchError, Observable, of, tap, throwError} from "rxjs";
 import {Team} from "../model/team";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Skill} from "../../skill/model/skill";
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +34,41 @@ export class TeamService {
   getAll(): Observable<Team[]>{
     return this.http.get<Team[]>(`${this.apiUrl}/teams`).pipe(
       tap(teamList => this._teamList$.next(teamList)),
+      catchError(this.handleError)
+    );
+  }
+  addTeam(team: Team): Observable<Team> {
+    return this.http.post<Team>(`${this.apiUrl}/teams`, team).pipe(
+      tap(newTeam => {
+        const updatedSkills = [...this._teamList$.getValue(), newTeam]
+        this._teamList$.next(updatedSkills);
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  deleteTeam(id: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/teams/${id}`).pipe(
+      tap(() => {
+        const updatedTeams = this._teamList$.getValue().filter(team => team.id !== id);
+        this._teamList$.next(updatedTeams)
+      }),
+      catchError(this.handleError)
+    )
+  }
+  getTeam(id: number): Observable<Team> {
+    return this.http.get<Team>(`${this.apiUrl}/teams/${id}`).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  updateTeam(id: number, team: Team): Observable<Team> {
+    return this.http.put<Team>(`${this.apiUrl}/teams/${id}`, team).pipe(
+      tap((updatedTeam: Team) => {
+        // Lokale Liste aktualisieren: Finde das Team-Objekt mit derselben ID und ersetze es
+        const updatedTeams = this._teamList$.getValue().map(t => t.id === id ? updatedTeam : t);
+        this._teamList$.next(updatedTeams);
+      }),
       catchError(this.handleError)
     );
   }
