40912a9de000d116a46f449b865fbd8031fb717c
Max Grimm <max.grimm@stud.h-da.de>
2024-09-19 11:57:42 +0200

    clean

--- bootcamp-angular/src/app/ui/delete-dialog/delete-dialog.component.ts
+++ bootcamp-angular/src/app/ui/delete-dialog/delete-dialog.component.ts
@@ -3,7 +3,7 @@ import {
   MAT_DIALOG_DATA,
   MatDialogRef,
 } from "@angular/material/dialog";
-import {DeleteDialogData} from "../../shared/delete-dialog-data";
+import {DeleteDialogData} from "./delete-dialog-data";
 
 
 @Component({
