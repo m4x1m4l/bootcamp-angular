7146bfbabbceffbf955e34c7ba605f9096913c0d
Max Grimm <max.grimm@stud.h-da.de>
2024-09-22 15:09:03 +0200

    added interface

---
+++ bootcamp-angular/src/app/page/employee/model/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  id?: number,
+  teamId: number,
+  teamName: string,
+  firstname: string,
+  lastname: string,
+  birthdate: Date,
+  salutation: string
+  trumpQuote?: string
+}
