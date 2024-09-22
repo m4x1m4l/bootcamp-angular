4d119bc86ee7dcec95a16889cf24d5dd0b87253c
Max Grimm <max.grimm@stud.h-da.de>
2024-09-22 15:26:13 +0200

    added employee.component

--- bootcamp-angular/src/app/page/employee/employee.component.ts
+++ bootcamp-angular/src/app/page/employee/employee.component.ts
@@ -1,9 +1,10 @@
 import {Component, inject} from '@angular/core';
-import {catchError, Observable, of} from "rxjs";
+import {catchError, Observable, of, tap} from "rxjs";
 import {MatDialog} from "@angular/material/dialog";
 import {Employee} from "./model/employee";
 import {EmployeeDataService} from "./service/employee-data.service";
 import {Router} from "@angular/router";
+import {DeleteDialogComponent} from "../../ui/delete-dialog/delete-dialog.component";
 
 @Component({
   selector: 'app-employee',
@@ -25,6 +26,37 @@ export class EmployeeComponent {
         console.error('Fehler beim Laden der Daten', error);
         return of([]); // Im Fehlerfall wird eine leere Liste zurückgegeben
       })
+    );
+  }
+
+  openDeleteDialog(toDelete: string) {
+    return this.dialog.open(DeleteDialogComponent, {
+      data: {
+        toDelete: toDelete
+      },
+      width: '400px',
+      enterAnimationDuration: '50ms',
+      exitAnimationDuration: '50ms',
+    });
+  }
+
+  deleteElement(id: number) {
+    const dialogRef = this.openDeleteDialog(`ID: ${id}`);
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) this.employeeDataService.deleteEmployee(id).pipe(
+        tap(() => console.log(`Employee mit ID ${id} gelöscht`)),
+        tap(() => this.router.navigateByUrl(this.router.url)),
+        catchError(error => {
+          console.error(`Fehler beim Löschen des Employees mit ID ${id}`, error);
+          return of(null); // Fehlerfall abfangen und nichts tun
+        })
+      ).subscribe();
+    })
+  }
+
+  onEdit(id: number) {
+    this.router.navigate(
+      ['/employee', id, 'edit']
     );
   }
 
